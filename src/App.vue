57a964e2a13c0ae63c5488084f000520386f263a
youssef-erremili <yousseferremili19@gmail.com>
2024-09-16 16:03:34 +0100

    WIP

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,25 @@
 
   <NavBar></NavBar>
   <HeaderSection></HeaderSection>
+  <main>
+    <FeatureSection></FeatureSection>
+    <ServiceSection></ServiceSection>
+  </main>
 
 </template>
 
 <script>
-import HeaderSection from "./components/HeaderSection.vue";
 import NavBar from "./components/NavBar.vue";
+import HeaderSection from "./components/HeaderSection.vue";
+import FeatureSection from "./components/FeatureSection.vue";
+import ServiceSection from "./components/ServiceSection.vue";
 
 export default {
   components: {
     NavBar,
-    HeaderSection
+    HeaderSection,
+    FeatureSection,
+    ServiceSection,
   }
 }
 </script>
