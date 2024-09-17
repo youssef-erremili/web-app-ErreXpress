f03b0b512ec9711a9b2e27c991b52459a9d4783c
youssef-erremili <yousseferremili19@gmail.com>
2024-09-17 18:07:47 +0100

    Create a custom link

---
+++ src/components/CustomLink.vue
@@ -0,0 +1,14 @@
+<template>
+    <a :href="url" rel="noopener noreferrer">
+        <slot></slot>
+    </a>
+</template>
+
+<script>
+export default {
+    name: "CustomLink",
+    props: [
+        "url",
+    ]
+}
+</script>
