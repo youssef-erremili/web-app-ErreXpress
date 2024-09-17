fa5e1ac4efc569f44451836580833c7717b9786c
youssef-erremili <yousseferremili19@gmail.com>
2024-09-17 18:11:17 +0100

    Create a HeroSection

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="main_hero text-center w-11/12 mx-auto mt-40">
+        <h4 class="text-base text-blue-800 capitalize font-medium">{{ heading }}</h4>
+        <h1 class="text-balance text-5xl my-2 capitalize font-semibold text-black text-opacity-90">
+            {{ title }}
+        </h1>
+        <p class="font-light text-balance text-xl text-opacity-80 lowercase text-gray-900">{{ slogan }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HeroSection",
+    props: [
+        "heading",
+        "title",
+        "slogan"
+    ]
+}
+</script>
