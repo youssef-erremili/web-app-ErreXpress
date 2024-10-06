87c8f920025e8c6010f5e241f71ced25834b428f
youssef-erremili <yousseferremili19@gmail.com>
2024-10-06 17:13:07 +0100

    Selet Components

---
+++ src/components/SelectInput.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="option">
+        <select :value="modelValue"
+            class="bg-gray-50 border border-gray-300 outline-none cursor-pointer text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2 px-2"
+            @change="$emit('update:modelValue', $event.target.value)">
+            <option v-for="(option, index) in options" :key="index" :value="option.value">
+                {{ option.text }}
+            </option>
+        </select>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SelectInput',
+    props: {
+        modelValue: {
+            type: String,
+            required: true
+        },
+        options: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
