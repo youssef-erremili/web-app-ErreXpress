729dc82f194dcca3e0c5c439a46d4767c4d33e5c
youssef-erremili <yousseferremili19@gmail.com>
2024-10-06 20:54:13 +0100

    Make Vue Routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,37 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+// import { createMemoryHistory, createRouter } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+import ServiceView from '@/views/ServiceView.vue'
+import TranslateView from '@/views/TranslateView.vue'
+import ContactView from '@/views/ContactView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: "home",
+    component: HomeView
+  },
+  {
+    path: '/services',
+    name: "service",
+    component: ServiceView
+  },
+  {
+    path: '/translate',
+    name: "translate",
+    component: TranslateView
+  },
+  {
+    path: '/contact',
+    name: "contact",
+    component: ContactView
+  },
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  history: createWebHistory(),
+  routes,
 })
 
 export default router
