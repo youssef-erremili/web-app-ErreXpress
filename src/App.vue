8c554c9eff1e4ad4a809f975e3cb3cdbc7080335
youssef-erremili <yousseferremili19@gmail.com>
2024-10-06 17:14:34 +0100

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,10 @@
 <template>
-  <header>
-    <NavBar></NavBar>
-    <HeaderSection></HeaderSection>
-  </header>
-  <main>
-    <FeatureSection></FeatureSection>
-    <ServiceSection></ServiceSection>
-  </main>
-  <footer>
-      <FooterSection></FooterSection>
-  </footer>
+    <NavBar />
+    <main>
+        <RouterView />
+    </main>
 </template>
 
 <script>
-import NavBar from "./components/NavBar.vue";
-import HeaderSection from "./components/HeaderSection.vue";
-import FeatureSection from "./components/FeatureSection.vue";
-import ServiceSection from "./components/ServiceSection.vue";
-import FooterSection from "./components/FooterSection.vue";
-
-export default {
-  components: {
-    NavBar,
-    HeaderSection,
-    FeatureSection,
-    ServiceSection,
-    FooterSection
-  }
-}
+export default {}
 </script>
