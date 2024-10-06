b27c3e9dae093f9f7652d90bb01c8149d4d6e81d
youssef-erremili <yousseferremili19@gmail.com>
2024-10-06 17:15:20 +0100

    WIP

--- src/main.js
+++ src/main.js
@@ -1,10 +1,28 @@
 import './index.css'
 import './assets/style.css'
-
+import router from './router';
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-const app = createApp(App)
+import NavBar from "./components/NavBar.vue";
+import HeaderSection from "./components/HeaderSection.vue";
+import HeroSection from "./components/HeroSection.vue";
+import FeatureSection from "./components/FeatureSection.vue";
+import ServiceSection from "./components/ServiceSection.vue";
+import FooterSection from "./components/FooterSection.vue";
+// import SelectInput from "./components/SelectInput.vue";
 
+const app = createApp(App)
+app.component("NavBar", NavBar)
+app.component("HeaderSection", HeaderSection)
+app.component("HeroSection", HeroSection)
+app.component("FeatureSection", FeatureSection)
+app.component("ServiceSection", ServiceSection)
+app.component("FooterSection", FooterSection)
+// app.component("SelectInput", SelectInput)
+
+
+
+app.use(router);
 app.mount('#app')
