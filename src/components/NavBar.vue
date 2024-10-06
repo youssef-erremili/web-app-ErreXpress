9553a88af3bb3ce64dcf4f2265cb03c50bfeccc6
youssef-erremili <yousseferremili19@gmail.com>
2024-10-06 17:18:25 +0100

    Remove links add routerLink

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,58 +1,54 @@
 <template>
-    <div class="p-5" id="navbarContainer">
-        <div id="navbar"  class="navbar flex justify-between items-center text-white w-8/12 mx-auto py-5 px-6 my-4 rounded-full shadow-lg bg-black">
-            <section class="logo">
-                <CustomLink :href="brandUrl" class="flex items-center space-x-3">
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                        {{ brandName }}
-                    </span>
-                </CustomLink>
-            </section>
-            <section class="links">
-                <ul class="flex">
-                    <li class="mx-2" v-for="(link, index) in removeLastLink()" :key="index">
-                        <CustomLink :href="link.url" class="font-light capitalize" >{{ link.text }}</CustomLink>
-                    </li>
-                </ul>
-            </section>
-            <section v-html="getLastLink()"></section>
+    <nav>
+        <div class="p-5" id="navbarContainer">
+            <div id="navbar"
+                class="navbar flex justify-between items-center text-white w-8/12 mx-auto py-5 px-6 my-4 rounded-full shadow-lg bg-black">
+                <section class="logo">
+                    <router-link to="/" class="font-light capitalize">
+                        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                            {{ brandName }}
+                        </span>
+                    </router-link>
+                </section>
+                <section class="links">
+                    <ul class="flex">
+                        <li class="mx-2">
+                            <router-link to="/" class="font-light capitalize">home</router-link>
+                        </li>
+                        <li class="mx-2">
+                            <router-link to="/services" class="font-light capitalize">Services</router-link>
+                        </li>
+                        <li class="mx-2">
+                            <router-link to="/contact" class="font-light capitalize">contact</router-link>
+                        </li>
+                        <li class="mx-2">
+                            <router-link to="/translate" class="font-light capitalize">translate</router-link>
+                        </li>
+                    </ul>
+                </section>
+                <section>
+                    <ul>
+                        <li class="mx-2">
+                            <router-link to="/" class="font-light capitalize">Get started</router-link>
+                        </li>
+                    </ul>
+                </section>
+            </div>
         </div>
-    </div>
+    </nav>
 </template>
 
 
 <script>
-
-import CustomLink from './CustomLink.vue';
-
 export default {
     name: "NavBar",
-    components: {
-        CustomLink,
-    },
     data() {
         return {
             temp: "",
             brandName: "Errehub",
-            brandUrl: "index.html",
-            links: [
-                { text: "Home", url: "index.html" },
-                { text: "Services", url: "services.html" },
-                { text: "Features", url: "features.html" },
-                { text: "contact", url: "contact.html" },
-                { text: "Get started", url: "getstarted.html" },
-            ]
+            brandUrl: "/"
         }
-    },
-    methods: {
-        removeLastLink() {
-            return this.links.slice(0, -1)
-        },
-        getLastLink() {
-            let lastLink = this.links.findLast(element => element.text.length > 0);
-            return `<a class="bg-white rounded-full p-3 text-black font-semibold" href="${lastLink.url}">${lastLink.text}</a>`;
-        }
-    },    
+    }
 }
 
 
