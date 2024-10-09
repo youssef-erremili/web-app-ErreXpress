6033cf9613ed18f4f30f7e9274153c8c001a9a04
youssef-erremili <yousseferremili19@gmail.com>
2024-10-09 01:21:40 +0100

    Create skeletonLoading component

---
+++ src/components/skeletonLoading.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <div role="status"
+            class="max-w-md p-4 space-y-4 border border-gray-200 divide-y rounded shadow animate-pulse divide-gray-700 md:p-6 ">
+            <div class="flex items-center justify-between">
+                <div>
+                    <div class="h-2.5 bg-gray-300 rounded-full   w-24 mb-2.5"></div>
+                    <div class="w-32 h-2 bg-gray-200 rounded-full  "></div>
+                </div>
+                <div class="h-2.5 bg-gray-300 rounded-full   w-12"></div>
+            </div>
+            <div class="flex items-center justify-between pt-4">
+                <div>
+                    <div class="h-2.5 bg-gray-300 rounded-full w-24 mb-2.5"></div>
+                    <div class="w-32 h-2 bg-gray-200 rounded-full  "></div>
+                </div>
+                <div class="h-2.5 bg-gray-300 rounded-full w-12"></div>
+            </div>
+            <div class="flex items-center justify-between pt-4">
+                <div>
+                    <div class="h-2.5 bg-gray-300 rounded-full w-24 mb-2.5"></div>
+                    <div class="w-32 h-2 bg-gray-200 rounded-full  "></div>
+                </div>
+                <div class="h-2.5 bg-gray-300 rounded-full w-12"></div>
+            </div>
+            <div class="flex items-center justify-between pt-4">
+                <div>
+                    <div class="h-2.5 bg-gray-300 rounded-full w-24 mb-2.5"></div>
+                    <div class="w-32 h-2 bg-gray-200 rounded-full  "></div>
+                </div>
+                <div class="h-2.5 bg-gray-300 rounded-full w-12"></div>
+            </div>
+            <div class="flex items-center justify-between pt-4">
+                <div>
+                    <div class="h-2.5 bg-gray-200 rounded-full w-24 mb-2.5"></div>
+                    <div class="w-32 h-2 bg-gray-200 rounded-full"></div>
+                </div>
+                <div class="h-2.5 bg-gray-300 rounded-full w-12"></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
