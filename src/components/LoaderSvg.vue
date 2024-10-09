8863a097b12e96009890fad0333914fd5508af25
youssef-erremili <yousseferremili19@gmail.com>
2024-10-09 01:20:54 +0100

    Create loader component

---
+++ src/components/LoaderSvg.vue
@@ -0,0 +1,24 @@
+<template>
+    <svg class='w-4 h-4 mr-2 stroke-white animate-spin ' viewBox='0 0 24 24' fill='none'
+        xmlns='http://www.w3.org/2000/svg'>
+        <g clip-path='url(#clip0_9023_61563_1)'>
+            <path
+                d='M14.6437 2.05426C11.9803 1.2966 9.01686 1.64245 6.50315 3.25548C1.85499 6.23817 0.504864 12.4242 3.48756 17.0724C6.47025 21.7205 12.6563 23.0706 17.3044 20.088C20.4971 18.0393 22.1338 14.4793 21.8792 10.9444'
+                stroke='stroke-current' stroke-width='1.4' stroke-linecap='round' class='my-path'>
+            </path>
+        </g>
+        <defs>
+            <clipPath id='clip0_9023_61563_1'>
+                <rect width='24' height='24' fill='white'></rect>
+            </clipPath>
+        </defs>
+    </svg>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
