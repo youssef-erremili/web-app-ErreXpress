0994e36eb9feddabc3ce4f4d845a2b4ea4bef5ea
youssef-erremili <yousseferremili19@gmail.com>
2024-10-12 16:51:42 +0100

    Create skeleton loading component

--- src/components/skeletonLoading.vue
+++ src/components/skeletonLoading.vue
@@ -1,32 +1,32 @@
 <template>
     <div>
         <div role="status"
-            class="max-w-md p-4 space-y-4 border border-gray-200 divide-y rounded shadow animate-pulse divide-gray-700 md:p-6 ">
+            class="p-4 space-y-4 border border-slate-100 divide-y rounded shadow animate-pulse divide-slate-200">
             <div class="flex items-center justify-between">
                 <div>
-                    <div class="h-2.5 bg-gray-300 rounded-full   w-24 mb-2.5"></div>
-                    <div class="w-32 h-2 bg-gray-200 rounded-full  "></div>
-                </div>
-                <div class="h-2.5 bg-gray-300 rounded-full   w-12"></div>
-            </div>
-            <div class="flex items-center justify-between pt-4">
-                <div>
                     <div class="h-2.5 bg-gray-300 rounded-full w-24 mb-2.5"></div>
-                    <div class="w-32 h-2 bg-gray-200 rounded-full  "></div>
+                    <div class="w-32 h-2 bg-gray-200 rounded-full"></div>
                 </div>
                 <div class="h-2.5 bg-gray-300 rounded-full w-12"></div>
             </div>
             <div class="flex items-center justify-between pt-4">
                 <div>
                     <div class="h-2.5 bg-gray-300 rounded-full w-24 mb-2.5"></div>
-                    <div class="w-32 h-2 bg-gray-200 rounded-full  "></div>
+                    <div class="w-32 h-2 bg-gray-200 rounded-full"></div>
                 </div>
                 <div class="h-2.5 bg-gray-300 rounded-full w-12"></div>
             </div>
             <div class="flex items-center justify-between pt-4">
                 <div>
                     <div class="h-2.5 bg-gray-300 rounded-full w-24 mb-2.5"></div>
-                    <div class="w-32 h-2 bg-gray-200 rounded-full  "></div>
+                    <div class="w-32 h-2 bg-gray-200 rounded-full"></div>
+                </div>
+                <div class="h-2.5 bg-gray-300 rounded-full w-12"></div>
+            </div>
+            <div class="flex items-center justify-between pt-4">
+                <div>
+                    <div class="h-2.5 bg-gray-300 rounded-full w-24 mb-2.5"></div>
+                    <div class="w-32 h-2 bg-gray-200 rounded-full"></div>
                 </div>
                 <div class="h-2.5 bg-gray-300 rounded-full w-12"></div>
             </div>
