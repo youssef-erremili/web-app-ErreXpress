a9105f8be8c7c2fdfddde740ba3dbb5528144f2b
youssef-erremili <yousseferremili19@gmail.com>
2024-10-12 16:52:37 +0100

    Change route's pages

--- src/router/index.js
+++ src/router/index.js
@@ -2,36 +2,41 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 // import { createMemoryHistory, createRouter } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
-import ServiceView from '@/views/ServiceView.vue'
+import ParaphraseView from '@/views/paraphraseView.vue'
 import TranslateView from '@/views/TranslateView.vue'
 import ContactView from '@/views/ContactView.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: "home",
-    component: HomeView
-  },
-  {
-    path: '/services',
-    name: "service",
-    component: ServiceView
-  },
-  {
-    path: '/translate',
-    name: "translate",
-    component: TranslateView
-  },
-  {
-    path: '/contact',
-    name: "contact",
-    component: ContactView
-  },
+    {
+        path: '/',
+        name: "home",
+        component: HomeView
+    },
+    {
+        path: '/paraphrase',
+        name: "paraphrase",
+        component: ParaphraseView
+    },
+    {
+        path: '/translation',
+        name: "translation",
+        component: TranslateView
+    },
+    {
+        path: '/correcter',
+        name: "correcter",
+        component: ContactView
+    },
+    {
+        path: '/contact',
+        name: "contact",
+        component: ContactView
+    },
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+    history: createWebHistory(),
+    routes,
 })
 
 export default router
