28cc7881163325efc9df6c870ac9cfad44a26e0e
youssef-erremili <yousseferremili19@gmail.com>
2024-10-12 16:55:01 +0100

    create custom button component

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,28 @@
+<template>
+    <button :type="buttontype" class="flex justify-center items-center">
+        <ion-icon class="mr-1" :name="buttonicon"></ion-icon>
+        {{ buttonname }}
+    </button>
+</template>
+
+<script>
+export default {
+    name: "Button",
+    props: {
+        buttonname: {
+            Type: String,
+            Required: true
+        },
+        buttonicon: {
+            Type: String,
+            Required: false,
+            default: ''
+        },
+        buttontype: {
+            Type: String,
+            Required: true,
+            default: 'button'
+        }
+    }
+}
+</script>
