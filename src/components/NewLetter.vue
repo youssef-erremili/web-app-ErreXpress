9c3276c94791978b84fad911c41fdf80d1387e61
youssef-erremili <yousseferremili19@gmail.com>
2024-10-23 17:57:22 +0100

    Create newletter component

---
+++ src/components/NewLetter.vue
@@ -0,0 +1,32 @@
+<template>
+    <section class="mt-48 max-w-screen-xl mx-auto">
+        <div class="mx-4 p-8 rounded-lg bg-gradient-to-tr from-[#4e54c8] to-[#8f94fb] sm:mx-8">
+            <div class="max-w-xl mx-auto">
+                <div class="space-y-4 text-center">
+
+                    <h1 class="text-3xl text-white font-semibold">
+                        Subscribe to our newsletter
+                    </h1>
+                    <p class="text-gray-300 leading-relaxed">
+                        Stay up to date with the roadmap progress, announcements and exclusive discounts feel free to
+                        sign up with your email.
+                    </p>
+                </div>
+                <div class="mt-5">
+                    <form class="max-w-md mx-auto flex items-center justify-center bg-white rounded-md p-1">
+                        <input type="email" placeholder="Enter your email"
+                            class="text-gray-500 w-full p-2.5 outline-none" />
+                        <button
+                            class="p-2.5 rounded-md text-white bg-indigo-600 outline-none shadow-md focus:shadow-none sm:px-5">
+                            Subscribe
+                        </button>
+                    </form>
+                    <p class="mt-3 mx-auto text-center max-w-lg text-[15px] text-gray-300">
+                        No spam ever, we are care about the protection of your data.
+                        Read our <a class="underline" href="javascript:void(0)"> Privacy Policy </a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
