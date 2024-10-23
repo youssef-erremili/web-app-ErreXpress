a442fd853c947a39a5433f8727d54c651959ed3e
youssef-erremili <yousseferremili19@gmail.com>
2024-10-23 20:48:26 +0100

    make some changes

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -3,14 +3,14 @@
         <header class="block relative mx-auto pt-5 rounded-md mb-6 w-11/12">
             <div class="text-center my-16 px-10">
                 <span class="flex items-center justify-evenly bg-white capitalize w-72 font-medium text-sm text-gray-700 rounded-full py-1.5 px-5 mb-3 mx-auto">
-                    <img src="../../public/images/icon-title.svg" alt="icon">
+                    <img src="/images/icon-title.svg" alt="icon">
                     {{ shortSlogan }}
                 </span>
-                <h1 class="text-6xl text-balance capitalize font-bold leading-tight">{{ theBigTitle }}</h1>
+                <h1 class="text-6xl text-balance capitalize font-bold text-slate-950 leading-tight">{{ Title }}</h1>
                 <p class="text-balance text-lg text-gray-950 my-3">{{ slogan }}</p>
             </div>
             <div class="mt-16 w-2/3 mx-auto">
-                <img class="mx-auto w-full rounded-t-xl rounded-tr-xl" src="../../public/images/hero-image.jpg"
+                <img class="mx-auto w-full rounded-t-xl rounded-tr-xl" src="/images/hero-image.jpg"
                     alt="hero image">
             </div>
         </header>
@@ -23,7 +23,7 @@ export default {
     data() {
         return {
             shortSlogan: "unlock the power of our tools",
-            theBigTitle: "Your Ultimate Destination for Translation and Grammar Perfection",
+            Title: "Your Ultimate Destination for Translation and Grammar Perfection",
             slogan: "Perfect your words, and effortlessly connect the  entire world with unmatched precision, clarity, and confidence in every communication",
         }
     }
