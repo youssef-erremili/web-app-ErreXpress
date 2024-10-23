07209be01f75366e3a5222a9f0ba20c0851d799b
youssef-erremili <yousseferremili19@gmail.com>
2024-10-23 20:51:36 +0100

    add error handling and improve code

--- src/views/CorrecterView.vue
+++ src/views/CorrecterView.vue
@@ -1,37 +1,33 @@
 <template>
     <div>
-        <hero-section :CustomId="'heading'" :CustomClass="'mt-20'" :heading="'Free grammer correcter Tool'"
-            :slogan="'Quickly correct any sentences, essays, emails, articles, and more'" />
+        <Breadcrumb />
+        <router-view />
     </div>
-    <!-- <button @click="grammarChecker()" type="button"
-        class="p-2 bg-indigo-500 rounded-md capitalize text-lg text-white ml-20 inline-block">Click Me!</button> -->
-
-    <!-- <textarea v-model="text" autocorrect="false" cols="30" rows="10"></textarea> -->
-    <!-- <div class="bg-white" contenteditable="true" v-html="text"></div> -->
     <main>
-        <form action="" @submit.prevent="grammarChecker()">
+        <form @submit.prevent="handleEmptyInp()">
             <div class="w-11/12 flex items-start justify-between p-1 mx-auto mt-12">
-                <div class="bg-white h-96 w-2/3 rounded-md overflow-hidden relative">
-                    <div class="flex items-center justify-between  p-1 px-4 border-b-2 border-b-gray-100">
-                        <section class="">
-                            <select name="" id="">
-                                <option value="">English</option>
-                                <option value="">English</option>
-                                <option value="">English</option>
+                <div class="bg-slate-50 h-96 w-2/3 rounded-md overflow-hidden relative">
+                    <div class="flex items-center justify-between  p-1 px-4 border-b-2 border-b-slate-200">
+                        <section>
+                            <select class="bg-slate-50">
+                                <option value="English">English</option>
+                                <option value="Arabic">Arabic</option>
+                                <option value="Spanish">Spanish</option>
                             </select>
                         </section>
                         <section class="p-3 px-4 flex items-center">
-                            <CustomButton class="text-sm text-slate-600" :buttontype="'button'" :buttonname="'Copy'"
-                                :buttonicon="'copy-outline'" />
-                            <CustomButton class="text-sm text-slate-600 ml-5" :buttontype="'button'"
+                            <CustomButton @click="copyCorretedText(content)" class="text-sm text-slate-600"
+                                type="button" :buttonname="'Copy'" :buttonicon="'copy-outline'" />
+                            <CustomButton @click="clearArea()" class="text-sm text-slate-600 ml-5" type="button"
                                 :buttonname="'Delete'" :buttonicon="'trash-outline'" />
                         </section>
                     </div>
-                    <div class="relative bg-white h-full">
-                        <InputField v-model="content" @input="countWordsAndChars()"
-                            class=" w-full h-full rounded-none" />
+                    <div class="relative h-full">
+                        <textarea v-model="content" @input="countWordsAndChars()" spellcheck="false"
+                            autocapitalize="off" autocorrect="off" autocomplete="off" placeholder="enter your text"
+                            class="bg-slate-50 w-full h-full border-none rounded-md py-4 px-3 pr-4 outline-none resize-none border border-gray-200 ml-1"></textarea>
                     </div>
-                    <div class="flex items-center  absolute left-0 bottom-0">
+                    <div class="flex items-center absolute left-0 bottom-0">
                         <p class="text-sm text-gray-400 capitalize py-2 px-4">
                             characters:
                             <span class="text-gray-900">{{ Characters }}</span>
@@ -41,65 +37,92 @@
                             <span class="text-gray-900">{{ Words }}</span>
                         </p>
                     </div>
-                    <div class="absolute right-2 bottom-1">
-                        <span
-                            class="bg-indigo-700 font-normal text-base rounded-full py-1 px-3 block text-white">4</span>
+                    <div class="absolute right-2 bottom-2">
+                        <CustomButton
+                            class="py-1.5 px-4 capitalize text-[17px] bg-indigo-500 font-light rounded-md mx-auto text-white"
+                            :buttonname="buttonValue" type="submit" :buttonicon="icon">
+                        </CustomButton>
+                        <LoaderSvg v-if="isLoading" class="absolute top-3 left-3.5" />
                     </div>
                 </div>
-                <div class=" bg-white h-96 w-2/6 rounded-md ml-2 overflow-hidden">
-                    <div class="flex items-center justify-around">
-                        <button
-                            class="w-full px-8 h-10 text-sm font-semibold text-slate-900 border-b-2 border-b-gray-300 capitalize">
-                            Correct 2
-                        </button>
-                        <button
-                            class="w-full px-8 h-10 text-sm font-semibold text-slate-900 border-b-2 border-b-gray-300 capitalize ml-1">
+                <div class="bg-slate-50 h-96 w-2/6 rounded-md ml-2 overflow-hidden">
+                    <div class="flex items-center justify-around mx-2">
+                        <div
+                            class="text-nowrap w-2/4 text-center py-4 text-sm font-semibold text-gray-600 border-b-2 border-b-slate-200 capitalize">
+                            Correct {{ errors.length }}
+                        </div>
+                        <div
+                            class="w-2/4 text-center py-4 text-sm font-semibold text-gray-600 border-b-2 border-b-slate-200 capitalize ml-4">
                             paraphrase
-                        </button>
+                        </div>
                     </div>
-                    <div class="bg-gray-50 rounded py-3 px-4 w-11/12 shadow-md my-4 flex items-center mx-auto">
-                        <p class="bg-indigo-500 text-base text-white font-semibold py-0.5 px-3.5 capitalize rounded-md">
-                            hello
+                    <div v-for="(error, index) in errors" :key="index"
+                        class="bg-white rounded py-3 px-4 w-11/12 shadow-md my-4 flex items-center mx-auto">
+                        <p class="bg-green-500 text-base text-white font-normal py-0.5 px-3 capitalize rounded-md">
+                            {{ error.suggestion }}
                         </p>
-                        <span class="text-xs text-gray-400 font-light ml-4 capitalize">grammer mistake</span>
-                    </div>
-                    <div class="bg-gray-50 rounded py-3 px-4 w-11/12 shadow-md my-4 flex items-center mx-auto">
-                        <p class="bg-indigo-500 text-base text-white font-semibold py-0.5 px-3.5 capitalize rounded-md">
-                            hello
-                        </p>
-                        <span class="text-xs text-gray-400 font-light ml-4 capitalize">Spelling mistake</span>
+                        <span class="text-sm text-slate-900 font-normal ml-4 capitalize">
+                            - {{ error.correctionType }}
+                        </span>
                     </div>
                 </div>
             </div>
-            <button class="bg-indigo-500" type="submit">click to
-                check</button>
         </form>
     </main>
-    <footer-section />
+    <footer>
+        <footer-section />
+    </footer>
 </template>
 
 <script>
+import Breadcrumb from '@/components/Breadcrumb.vue';
 import CustomButton from '@/components/CustomButton.vue';
+import LoaderSvg from '@/components/LoaderSvg.vue';
+
+import { toast } from "vue3-toastify";
+import "vue3-toastify/dist/index.css";
 
 export default {
-    components: {
-        CustomButton
-    },
     name: "CorrecterView",
+    components: {
+        CustomButton,
+        LoaderSvg,
+        Breadcrumb
+    },
     data() {
         return {
             content: "",
-            isHidden: true,
+            Isdisabled: false,
+            isLoading: false,
+            icon: "flash-outline",
+            buttonValue: "check",
             exChange: 0,
             limit: 500,
             Characters: 0,
             Words: 0,
             wordsArray: [],
-            error: []
+            corrections: [],
+            errors: [],
         };
     },
 
     methods: {
+        handleEmptyInp() {
+            if (this.content.trim() === '') {
+                toast("Type text to correct!", {
+                    "theme": "colored",
+                    "type": "warning",
+                    "autoClose": 2000,
+                    "position": "bottom-right",
+                    "hideProgressBar": true,
+                    "transition": "slide",
+                    "dangerouslyHTMLString": true
+                })
+            } else {
+                return this.grammarChecker();
+            }
+        },
+
         countWordsAndChars() {
             this.Characters = this.content.length;
             if (this.content.trim() === "") {
@@ -111,38 +134,160 @@ export default {
             }
         },
 
-        async grammarChecker() {
-            // const url = "https://ginger4.p.rapidapi.com/correction?lang=US&generateRecommendations=false&flagInfomralLanguage=true";
-            const apiKey = "ae3806ec64mshe6e8fcce507b60dp1207d2jsn6b0b16528c1e"; // Ensure this key is valid
-            const textToCheck = "Frree gramemer correceter Toeol"; // Example text
+        // handle grammar requests
+        grammarChecker() {
+            const url = 'https://api.ai21.com/studio/v1/gec';
+            const apiKey = "aMxrXBtMLyPzH48UIXrl1oMZqkroRrVh";
 
-            const url = 'https://ginger4.p.rapidapi.com/correction?lang=US&generateRecommendations=false&flagInfomralLanguage=true';
-            const options = {
+            this.icon = "no-thing"
+            this.buttonValue = "loading..."
+            this.isLoading = true
+            toast("Your content is being prepared!", {
+                "theme": "colored",
+                "type": "info",
+                "autoClose": 2000,
+                "position": "bottom-right",
+                "hideProgressBar": true,
+                "transition": "slide",
+                "dangerouslyHTMLString": true
+            })
+
+            fetch(url, {
                 method: 'POST',
                 headers: {
-                    'x-rapidapi-key': apiKey, // replace with your key
-                    'x-rapidapi-host': 'ginger4.p.rapidapi.com',
-                    'Content-Type': 'text/plain',
-                    'Accept-Encoding': 'identity',
+                    'Authorization': `Bearer ${apiKey}`,
+                    'Content-Type': 'application/json'
                 },
-                body: "Frree gramemer correceter Toeol",
-            };
+                body: JSON.stringify({
+                    "text": this.content
+                })
+            })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok ' + response.statusText);
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    if (data.corrections.length == 0) {
+                        this.errors = []
+                        this.buttonValue = "It's all correct"
+                        this.isLoading = false
+                        this.icon = "flash-outline"
+                        toast("Everything is correct!", {
+                            "theme": "colored",
+                            "type": "success",
+                            "autoClose": 2000,
+                            "position": "bottom-right",
+                            "hideProgressBar": true,
+                            "transition": "slide",
+                            "dangerouslyHTMLString": true
+                        })
+                        setTimeout(() => {
+                            this.buttonValue = "check"
+                        }, 4000);
+                    }
+                    else {
+                        this.errors = []
+                        this.buttonValue = "check"
+                        this.isLoading = false
+                        this.icon = "flash-outline"
+                        this.handleCorrections(data.corrections)
+                        this.correctError(data.corrections)
+                    }
+                })
+                .catch(error => {
+                    this.buttonValue = "check"
+                    this.isLoading = false
+                    this.icon = "flash-outline"
+                    console.error('There was a problem with the fetch operation:', error);
+                });
+        },
 
-            try {
-                const response = await fetch(url, options);
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
-                const result_1 = await response.text();
-                return result_1;
-            } catch (error) {
-                console.error(error);
-                throw error; // Rethrow the error for further handling if needed
+        // store corrections to ARRAY
+        handleCorrections(data) {
+            data.forEach((element) => {
+                this.errors.push({
+                    "suggestion": element.suggestion,
+                    "correctionType": element.correctionType
+                })
+            });
+        },
+
+
+        correctError(data) {
+            let correctedContent = this.content;
+            data.forEach(element => {
+                const regex = new RegExp(element.originalText, 'g');
+                correctedContent = correctedContent.replace(regex, element.suggestion);
+            });
+            toast("Text has been corrected!", {
+                "theme": "colored",
+                "type": "success",
+                "autoClose": 2000,
+                "position": "bottom-right",
+                "hideProgressBar": true,
+                "transition": "slide",
+                "dangerouslyHTMLString": true
+            })
+            this.content = correctedContent;
+        },
+
+        // Clear textarea
+        clearArea() {
+            if (this.content !== "") {
+                this.content = ""
+                this.errors = []
+                toast("The input is now blank!", {
+                    "theme": "colored",
+                    "type": "success",
+                    "autoClose": 2000,
+                    "position": "bottom-right",
+                    "hideProgressBar": true,
+                    "transition": "slide",
+                    "dangerouslyHTMLString": true
+                })
+            }
+        },
+
+        // Copy function
+        copyCorretedText(text) {
+            if (this.content !== "") {
+                navigator.clipboard.writeText(text)
+                    .then(() => {
+                        toast(`Text copied: "${text}"`, {
+                            "theme": "colored",
+                            "type": "success",
+                            "autoClose": 2000,
+                            "position": "bottom-right",
+                            "hideProgressBar": true,
+                            "transition": "slide",
+                            "dangerouslyHTMLString": true
+                        })
+                    })
+                    .catch(err => {
+                        toast(`Could not copy text! ${err}`, {
+                            "theme": "colored",
+                            "type": "error",
+                            "autoClose": 2000,
+                            "position": "bottom-right",
+                            "hideProgressBar": true,
+                            "transition": "slide",
+                            "dangerouslyHTMLString": true
+                        })
+                    });
+            } else {
+                toast(`There's nothing available to copy!`, {
+                    "theme": "colored",
+                    "type": "error",
+                    "autoClose": 2000,
+                    "position": "bottom-right",
+                    "hideProgressBar": true,
+                    "transition": "slide",
+                    "dangerouslyHTMLString": true
+                })
             }
         }
-        // }
-        // ("x")
-
     }
 }
 </script>
