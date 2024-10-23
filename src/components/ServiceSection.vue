95c1bc19350f7186216f824a786542608f6abf18
youssef-erremili <yousseferremili19@gmail.com>
2024-10-23 20:52:22 +0100

    add error handling and improve code

--- src/components/ServiceSection.vue
+++ src/components/ServiceSection.vue
@@ -1,14 +1,20 @@
 <template>
-    <heroSection :heading="titles[0].heading" :title="titles[0].title" :slogan="titles[0].content"/>
-    <div class="w-11/12 mx-auto flex flex-col">
-        <div v-for="(item, index) in ServicesContent" :key="index" :class="['box_service flex items-center justify-center', reverseBox(index) ? 'flex-row-reverse ml-12' : '']">
-            <div class="w-1/2">
-                <img :src="item.image" class="w-3/" :alt="`Image of ${item.title}`">
-            </div>
-            <div class="w-1/2">
-                <h2 class="text-4xl text-gray-950 text-opacity-90 font-semibold">{{ item.title }}</h2>
-                <p class="w-fit inline-block my-4 text-opacity-70 lowercase text-balance font-normal text-lg text-gray-900">{{ item.content }}</p>
-                <!-- <CustomLink :href="item.url" class="px-8 py-2 capitalize inline-block leading-none rounded-sm text-lg font-light bg-indigo-700 text-white">{{ item.url_text }}</CustomLink> -->
+    <div class="mt-32">
+        <heroSection :heading="titles[0].heading" :title="titles[0].title" :slogan="titles[0].content" />
+        <div class="w-11/12 mx-auto flex flex-col">
+            <div v-for="(item, index) in ServicesContent" :key="index"
+                :class="['box_service flex items-center justify-center', reverseBox(index) ? 'flex-row-reverse ml-12' : '']">
+                <div class="w-1/2">
+                    <img :src="item.image" class="w-3/" :alt="`Image of ${item.title}`">
+                </div>
+                <div class="w-1/2">
+                    <h2 class="text-4xl text-gray-950 text-opacity-90 font-semibold">{{ item.title }}</h2>
+                    <p
+                        class="w-fit inline-block my-4 text-opacity-70 lowercase text-balance font-normal text-lg text-gray-900">
+                        {{ item.content }}
+                    </p>
+                    <router-link :to="item.url" class="bg-indigo-500 text-white capitalize py-2 px-3 rounded-md inline-block my-1">{{ item.value }}</router-link>
+                </div>
             </div>
         </div>
     </div>
@@ -25,30 +31,30 @@ export default {
                 {
                     heading: "Services",
                     title: "provide many services that cover all you needs",
-                    content: "Discover the amazing  features of our website translator that make your multilingual journey easier! Our translator helps  you communicate effortlessly, making work simpler and opening up a world of seamless communication."
+                    content: "Explore our services that simplify communication! Whether you need translation, clear expression of your ideas, rephrasing, or grammar checks, we're here to help you work more efficiently and connect effortlessly."
                 },
             ],
             ServicesContent: [
                 {
-                    image: "../../public/images/easy.webp",
-                    title: "Speedy Translation Service",
-                    content: "Experience the best of both worlds with our exceptional translation service - where speed meets precision.",
-                    url: "test.com",
-                    url_text: "Get Started"
+                    image: "/images/easy.webp",
+                    title: "Translation with AI",
+                    content: "Translate your text easily with our service! Whether it's sentences, essays, emails, or articles, we help you share your ideas in another language. Great for school, work, or chatting with friends, we make sure your message is clear and makes sense.",
+                    url: "/translation",
+                    value: "Get started"
                 },
                 {
-                    image: "../../public/images/exportpdf.webp",
-                    title: "Quality Translation Service",
-                    content: "Make your words stand out with our promise of top-notch quality. We talk about excellence, Your message matters.",
-                    url: "test.com",
-                    url_text: "Get Started"
+                    image: "/images/exportpdf.webp",
+                    title: "Paraphrasing",
+                    content: "Need to change up your writing? We can help you rephrase sentences, essays, emails, articles, and more. Our service makes it easy to express your thoughts in a new way while keeping the original meaning. Perfect for school assignments.",
+                    url: "/paraphrase",
+                    value: "Get started"
                 },
                 {
-                    image: "../../public/images/resources.webp",
-                    title: "Translation Always Updated",
-                    content: "Stay in the now with our translations, always fresh, always up-to-date. Your content evolves, and so do we.",
-                    url: "test.com",
-                    url_text: "Get Started"
+                    image: "/images/resources.webp",
+                    title: "Grammar Checker",
+                    content: "Make sure your writing is error-free with our grammar checker! Quickly spot and correct mistakes in sentences, essays, emails, and articles. Whether you're writing for school, work, or personal use, we help you polish your text so it’    s clear and professional.",
+                    url: "/correcter",
+                    value: "Get started"
                 },
             ]
         }
